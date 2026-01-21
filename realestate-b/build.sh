54ed69cea3d9bed35530b603a365b62cfad44cea
Adrian Kisa <adrianchangwany@gmail.com>
2026-01-21 22:26:27 +0300

    fixed build

--- realestate-b/build.sh
+++ realestate-b/build.sh
@@ -6,3 +6,14 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='Hayes').exists():
+    User.objects.create_superuser('Hayes', 'hayes@yahoo.com', '${DJANGO_SUPERUSER_PASSWORD:-defaultpassword123qwert}')
+    print("Superuser created successfully!")
+else:
+    print("Superuser already exists.")
+EOF
